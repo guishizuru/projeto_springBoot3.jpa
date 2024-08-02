aaab932ddbc1d56fc1d5f6a5e95084431b0da9d3
Guilherme Shizuru <141662172+guishizuru@users.noreply.github.com>
2024-08-02 10:49:10 -0300

    first commit

---
+++ projetoSpringCursoNelio/src/main/java/com/projetoSpringCursoNelio/projetoSpringCursoNelio/ProjetoSpringCursoNelioApplication.java
@@ -0,0 +1,13 @@
+package com.projetoSpringCursoNelio.projetoSpringCursoNelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringCursoNelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringCursoNelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoSpringCursoNelio/projetoSpringCursoNelio/ProjetoSpringCursoNelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoSpringCursoNelio.projetoSpringCursoNelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringCursoNelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
