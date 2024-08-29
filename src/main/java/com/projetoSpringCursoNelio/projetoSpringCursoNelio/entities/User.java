2ea03d294bb869c84d4000a5e9d08bcf162516e3
Guilherme Shizuru <141662172+guishizuru@users.noreply.github.com>
2024-08-29 12:06:54 -0300

    insercao de @ para comunicar com o banco

--- src/main/java/com/projetoSpringCursoNelio/projetoSpringCursoNelio/entities/User.java
+++ src/main/java/com/projetoSpringCursoNelio/projetoSpringCursoNelio/entities/User.java
@@ -1,5 +1,6 @@
 package com.projetoSpringCursoNelio.projetoSpringCursoNelio.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -13,13 +14,21 @@ import java.io.Serializable;
 @Setter
 @EqualsAndHashCode
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
 
+    public User() {
+
+    }
 }
