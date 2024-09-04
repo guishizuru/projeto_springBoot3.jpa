24d7a7ae16b640c8bc3f3b01942f3912cd92b5c6
guilherme.shizuru <guilherme.shizuru@gmail.com>
2024-09-04 09:59:52 -0300

    utilizacao paymente one to one

--- src/main/java/com/projetoSpringCursoNelio/projetoSpringCursoNelio/entities/Order.java
+++ src/main/java/com/projetoSpringCursoNelio/projetoSpringCursoNelio/entities/Order.java
@@ -42,6 +42,8 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment ;
 
     public Order() {
 
